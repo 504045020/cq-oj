0f03b715c0b7949316c56789380d07f5c5d16e1d
chenqi <504045020@qq.com>
2024-07-06 15:19:08 +0800

    去除掉无用注释

---
+++ src/main/java/com/cq/oj/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.cq.oj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cq.oj.model.entity.User;
+
+/**
+* @author 陈起
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-07-05 16:14:14
+* @Entity com.cq.oj.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
