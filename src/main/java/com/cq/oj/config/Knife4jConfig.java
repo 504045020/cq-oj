ed937cf1acfcdb0df90f1e2cc7d7d1629f2a3707
chenqi <504045020@qq.com>
2024-07-07 18:28:28 +0800

    wagger

--- src/main/java/com/cq/oj/config/Knife4jConfig.java
+++ src/main/java/com/cq/oj/config/Knife4jConfig.java
@@ -13,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /***
  * Knife4j 接口文档配置
  * https://doc.xiaominfo.com/knife4j/documentation/get_start.html
+ * 本地 http://localhost:7001/doc.html#/home
  */
 @Configuration
 @EnableSwagger2
