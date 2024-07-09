1e7d7ab35100d98ef8ba71d9f2290faf7cd054a5
chenqi <504045020@qq.com>
2024-07-09 15:17:16 +0800

    分页

---
+++ src/main/java/com/cq/oj/constant/CommonConstant.java
@@ -0,0 +1,13 @@
+package com.cq.oj.constant;
+
+public interface CommonConstant {
+    /**
+     * 升序
+     */
+    String SORT_ORDER_ASC = "ascend";
+
+    /**
+     * 降序
+     */
+    String SORT_ORDER_DESC = " descend";
+}
